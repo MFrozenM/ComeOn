df8660eedc464412fa87cf93e88cfe44c88d28a6
MMahdiMB <mmmmahdimmm@Gmail.com>
2022-06-18 00:29:28 +0430

    implement login page

---
+++ src/pages/login/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './login.module.css'
+import Box from "../../components/login/box/box";
+
+const Login = () => {
+    return (
+        <div className={classes.container}>
+            <Box/>
+        </div>
+    );
+};
+
+export default Login;
